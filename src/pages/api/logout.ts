00542c0573064e0d8cd70b50251e84ca16c3da35
bossoq <boss.zz@me.com>
2021-08-16 14:24:18 +0700

    add expire to cookie

--- src/pages/api/logout.ts
+++ src/pages/api/logout.ts
@@ -2,10 +2,11 @@ import { NextApiRequest, NextApiResponse } from 'next'
 import { setCookie } from '../../utils/cookies'
 
 const logout = (req: NextApiRequest, res: NextApiResponse<any>) => {
-  setCookie(res, 'accesstoken', '', {
+  setCookie(res, 'accesstoken', 'invalid', {
     httpOnly: true,
     sameSite: 'lax',
     path: '/',
+    maxAge: 0,
   })
 
   res.status(200).json({ success: true })
