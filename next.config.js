3733af5bbe321201951097a6939fdf5d4470e461
bossoq <boss.zz@me.com>
2021-08-16 10:50:03 +0700

    add allow domains for images

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ['supapanya.com', 'ceoyctjctahurvmlupfh.supabase.in']
+    domains: ['supapanya.com', 'vod.supapanya.com', 'ceoyctjctahurvmlupfh.supabase.in']
   },
 }
