540e33364ec4e9c06f8f71b4c780cbbf2a70e08a
bossoq <boss.zz@me.com>
2021-08-16 13:01:56 +0700

    fix cookies

--- src/pages/api/islogin.ts
+++ src/pages/api/islogin.ts
@@ -1,27 +1,21 @@
 import jwt from 'jwt-simple'
-import Cookies from 'js-cookie'
 import { secret } from '../../utils/auth'
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 const islogin = (req: NextApiRequest, res: NextApiResponse<any>) => {
-  // const Cookies = require('cookies')
-  // const cookies = new Cookies(req, res)
-  // const accessToken: string = cookies.get('accesstoken')
-  const accessToken: string = Cookies.get('accesstoken') || ''
+  const Cookies = require('cookies')
+  const cookies = new Cookies(req, res)
+  const accessToken: string = cookies.get('accesstoken')
   try {
     const decryptData = jwt.decode(accessToken, secret)
     res.status(200).json({ isLoggedIn: true, ...decryptData })
   } catch (err: any) {
     console.error('isLogin: ' + err.message)
-<<<<<<< HEAD
-    // cookies.set('accesstoken', '', {
-    //   httpOnly: true,
-    //   sameSite: 'lax',
-    // })
-    Cookies.remove('accesstoken')
-=======
+    cookies.set('accesstoken', '', {
+      httpOnly: true,
+      sameSite: 'lax',
+    })
     cookies.set('accesstoken', '')
->>>>>>> parent of de5377a (Fix cookies bugs)
     res.status(200).json({ isLoggedIn: false })
   }
 }
--- src/pages/api/logout.ts
+++ src/pages/api/logout.ts
@@ -1,19 +1,14 @@
 import { NextApiRequest, NextApiResponse } from 'next'
-import Cookies from 'js-cookie'
 
 const logout = (req: NextApiRequest, res: NextApiResponse<any>) => {
-  // const Cookies = require('cookies')
-  // const cookies = new Cookies(req, res)
+  const Cookies = require('cookies')
+  const cookies = new Cookies(req, res)
 
-<<<<<<< HEAD
-  // cookies.set('accesstoken', '', {
-  //   httpOnly: true,
-  //   sameSite: 'lax',
-  // })
-  Cookies.remove('accesstoken')
-=======
-  cookies.set('accesstoken')
->>>>>>> parent of de5377a (Fix cookies bugs)
+  cookies.set('accesstoken', '', {
+    httpOnly: true,
+    sameSite: 'lax',
+  })
+  cookies.set('accesstoken', '')
 
   res.status(200).json({ success: true })
 }
